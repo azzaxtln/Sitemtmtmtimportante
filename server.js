a7277ff7202bc3b6f72e4532b64b538744bf47dc
azzaxtln <130991508+azzaxtln@users.noreply.github.com>
2024-03-14 00:45:50 -0300

    aaaaaaaaaaa

--- server.js
+++ server.js
@@ -7,23 +7,19 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Configure o middleware para servir arquivos estáticos a partir da pasta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rota para lidar com solicitações GET para a raiz do servidor
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-// Rota para lidar com o clique no botão
 app.post('/notify', (req, res) => {
-    // Token do seu bot do Telegram
-    const botToken = '6899131659:AAFsdGzI1A39a3JVP09ZHBEUk20ZjQB-L84'; // Substitua pelo seu token
+    const botToken = '6899131659:AAFsdGzI1A39a3JVP09ZHBEUk20ZjQB-L84'; 
     
-    // ID do chat para enviar a notificação
-    const chatId = '6023968992'; // Substitua pelo seu ID de chat
     
-    // Envie uma solicitação de notificação para o seu bot Telegram
+    const chatId = '6023968992'; 
+    
+    
     fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
         method: 'POST',
         headers: {
@@ -45,7 +41,7 @@ app.post('/notify', (req, res) => {
     });
 });
 
-// Inicie o servidor
+
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
